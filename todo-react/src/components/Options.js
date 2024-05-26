858aaa92fc00edfe8d6a1ed7f7b0522b42d68466
ahmedelkarra <153688104+ahmedelkarra@users.noreply.github.com>
2024-05-26 20:58:16 +0200

    Options.js

--- todo-react/src/components/Options.js
+++ todo-react/src/components/Options.js
@@ -22,7 +22,7 @@ const OptionsUpdate = () => {
 const Update = async (task) => {
     const id = getData.taskId
     console.log(getData);
-    await axios.patch(`http://localhost:4000/options/${id}`, { changeTask: task || getData.task, type: `task` }, { headers: { Authorization: localStorage.getItem('token') } })
+    await axios.patch(`https://to-do-app-mern-bf0eb3deb0e2.herokuapp.com/options/${id}`, { changeTask: task || getData.task, type: `task` }, { headers: { Authorization: localStorage.getItem('token') } })
         .then((e) => {
             console.log(e)
         })
@@ -34,14 +34,14 @@ const toDone = async () => {
     const id = getData.taskId
     let statusIsDone = false
     getData.isDone ? statusIsDone = false : statusIsDone = true
-    await axios.patch(`http://localhost:4000/options/${id}`, { isDone: statusIsDone, type: 'isDone' }, { headers: { Authorization: localStorage.getItem('token') } })
+    await axios.patch(`https://to-do-app-mern-bf0eb3deb0e2.herokuapp.com/options/${id}`, { isDone: statusIsDone, type: 'isDone' }, { headers: { Authorization: localStorage.getItem('token') } })
         .then((e) => console.log("Done To"))
         .catch((err) => console.error(err))
 }
 
 const Delete = async () => {
     const id = getData.taskId
-    await axios.delete(`http://localhost:4000/options/${id}`, { headers: { Authorization: localStorage.getItem('token') } })
+    await axios.delete(`https://to-do-app-mern-bf0eb3deb0e2.herokuapp.com/options/${id}`, { headers: { Authorization: localStorage.getItem('token') } })
         .then((e) => console.log(e))
         .catch((err) => console.error(err))
     console.log(getData);
